35b015e7773de11c9761cda0dcd45a3156e696e3
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-08-12 13:58:41 +0200

    category component $ routing

---
+++ src/views/Category.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="w3-hide-large" style="margin-top:83px"></div>
+    <header class="w3-container w3-xlarge">
+      <p class="w3-left">{{title}}</p>
+      <p class="w3-right">
+        <i class="fa fa-shopping-cart w3-margin-right right"></i>
+        <i class="fa fa-search"></i>
+      </p>
+    </header>
+    <div v-if="items">
+      <div class="w3-container w3-text-grey" id="jeans">
+        <p>{{rand}} items</p>
+      </div>
+      <div class="w3-container w3-grayscale nopadding">
+        <div
+          class="w3-col l3 s6 item w3-container"
+          v-for="(item, index) in getRandomItems(rand)"
+          v-bind:key="index"
+        >
+          <img :src="item.image" style="width:100%" />
+          <div class="w3-display-middle w3-display-hover" style="z-index: 3">
+            <button class="w3-button w3-black">
+              Buy now
+              <i class="fa fa-shopping-cart"></i>
+            </button>
+          </div>
+          <p>
+            {{item.product_name}}
+            <br />
+            <b>{{item.price}}</b>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div style="height: 400px; font-size: 3em; color: grey" v-else>Retrieving items for you</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'category',
+  computed: {
+    ...mapState(['items']),
+  },
+  watch: {
+    '$route.params.cat': function x() {
+      this.reload();
+    },
+  },
+  data() {
+    return {
+      title: null,
+      rand: null,
+    };
+  },
+  mounted() {
+    this.title = this.$route.params.cat;
+    this.rand = Math.floor(Math.random() * 10 + 7);
+  },
+  methods: {
+    getRandomItems(items) {
+      const ret = [];
+      const indexes = [];
+      const arrLength = 30;
+      while (ret.length < items) {
+        const i = Math.floor(Math.random() * arrLength);
+        if (indexes.indexOf(i) === -1) {
+          indexes[indexes.length] = i;
+          ret[ret.length] = this.items[i];
+        }
+      }
+      return ret;
+    },
+    reload() {
+      this.title = this.$route.params.cat;
+      this.rand = Math.floor(Math.random() * 10 + 7);
+    },
+  },
+};
+</script>
+
+<style>
+.item {
+  padding-left: 16px;
+  padding-right: 16px;
+  min-height: 360px;
+}
+
+.nopadding {
+  padding: 0px;
+}
+
+.right {
+  padding-right: 6.5px;
+}
+</style>
