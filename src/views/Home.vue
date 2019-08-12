37f9742d1f302723fcacd9fab1b2bad56765c22d
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-08-12 13:58:51 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w3-main" style="margin-left: 250px">
+  <div>
     <div class="w3-hide-large" style="margin-top:83px"></div>
     <header class="w3-container w3-xlarge">
       <p class="w3-left">Welcome</p>
@@ -21,54 +21,72 @@
     </div>
     <div v-if="items">
       <div class="w3-container w3-text-grey" id="jeans">
-        <p>{{items.length}} items</p>
+        <p>{{rand}} items</p>
       </div>
       <div class="w3-container w3-grayscale nopadding">
-        <div class="w3-col l3 s6 item" v-for="item in items" v-bind:key="item.id">
-          <img :src="item.image" style="width:100%" />
-          <p>
-            {{item.product_name}}
-            <br />
-            <b>{{item.price}}</b>
-          </p>
+        <div class="w3-col l3 s6" v-for="(item, index) in getRandomItems(rand)" v-bind:key="index">
+          <div class="w3-container item trigger">
+            <img :src="item.image" style="width: 100%" class="img-trigger" />
+            <div class="onHover">
+              <button class="w3-button w3-black" v-on:click="itemlog(item)">
+                Buy now
+                <i class="fa fa-shopping-cart"></i>
+              </button>
+            </div>
+            <p>
+              {{item.product_name}}
+              <br />
+              <b>{{item.price}}</b>
+            </p>
+          </div>
         </div>
       </div>
     </div>
     <div style="height: 400px; font-size: 3em; color: grey" v-else>Retrieving items for you</div>
-    <Footer />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import Footer from '@/components/Footer.vue';
-import API from '@/lib/API';
 
 export default {
   name: 'home',
-  components: {
-    Footer,
-  },
   computed: {
     ...mapState(['items']),
   },
   data() {
     return {
-      apidata: null,
+      rand: null,
+      active: false,
     };
   },
   mounted() {
-    API.getData().then((result) => {
-      this.apidata = result;
-    });
+    this.rand = Math.floor(Math.random() * 10 + 7);
+  },
+  methods: {
+    getRandomItems(items) {
+      const ret = [];
+      const indexes = [];
+      const arrLength = 30;
+      while (ret.length < items) {
+        const i = Math.floor(Math.random() * arrLength);
+        if (indexes.indexOf(i) === -1) {
+          indexes[indexes.length] = i;
+          ret[ret.length] = this.items[i];
+        }
+      }
+      return ret;
+    },
+    itemlog() {
+      // console.log(`'clicked: '${item.id}`);
+    },
   },
 };
 </script>
 
 <style>
 .item {
-  padding-left: 16px;
-  padding-right: 16px;
+  padding: 0;
   min-height: 360px;
 }
 
@@ -78,5 +96,31 @@ export default {
 
 .right {
   padding-right: 6.5px;
+}
+
+.onHover {
+  display: none;
+  position: relative;
+  top: -100px;
+  left: 63px;
+  margin: -19px;
+  padding: 0px;
+}
+
+.onHover:hover {
+  display: block;
+}
+
+.trigger:hover + .onHover {
+  display: block;
+}
+
+.img-trigger:hover + .onHover {
+  display: block;
+}
+
+.trigger:hover {
+  position: relative;
+  top: -4px;
 }
 </style>
